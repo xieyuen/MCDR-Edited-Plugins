fb0e58db9bd9a3b864baa5a467cb852c09003fcb
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-06-23 08:37:27 +0800

    update

--- StartStopHelper-v0.1.0/start_stop_helper_r_changed/__init__.py
+++ StartStopHelper-v0.1.0/start_stop_helper_r_changed/__init__.py
@@ -4,7 +4,7 @@ import time
 
 from typing import Dict
 
-from mcdreforged.api.types import PluginServerInterface
+from mcdreforged.api.types import PluginServerInterface as mcdrserver
 from mcdreforged.api.command import *
 from mcdreforged.api.utils import Serializable
 
@@ -22,23 +22,41 @@ class Config(Serializable):
 
 config: Config
 
-def stop_server(server: PluginServerInterface):
-    server.stop()
+def stop_server():
+    mcdrserver.stop()
     time.sleep(10)
-    if server.is_server_running()==True: time.sleep(60)
-    if server.is_server_running() == True:
-        server.logger.warning("服务器似乎并没有关闭...")
-        server.logger.info("10s后服务端进程将被杀死.")
-        time.sleep(10)
-        server.set_exit_after_stop_flag(False)
-        server.kill()
-    server.logger.info("服务端已关闭.")
+    if mcdrserver.is_server_running()==True: 
+        time.sleep(60)
+        if mcdrserver.is_server_running() == True:
+            mcdrserver.logger.warning("服务器似乎并没有关闭...")
+            mcdrserver.logger.info("10s后服务端进程将被杀死.")
+            time.sleep(10)
+            mcdrserver.set_exit_after_stop_flag(False)
+            if mcdrserver.is_server_running() == False: 
+                mcdrserver.logger.info("服务端已关闭")
+            else:
+                mcdrserver.set_exit_after_stop_flag(False)
+                mcdrserver.kill()
+    mcdrserver.logger.info("服务端已关闭.")
     return
+
+def stop_exit_server():
+    stop_server()
+    mcdrserver.exit()
+
+def restart_server():
+    stop_server()
+    mcdrserver.start()
 
 def on_load(server: PluginServerInterface, prev_module):
     global config
     config = server.load_config_simple('config.json', target_class=Config)
     permissions = config.permissions
+    mcdrserver.logger.info("插件已加载")
+    mcdrserver.logger.warning("你使用的并非原版！而是 xieyuen 更改版!")
+    mcdrserver.logger.info("若需要原版，请在下面的网址下载（或者用MPM）")
+    mcdrserver.logger.info("https://www.mcdreforged.org/plugins/start_stop_helper_r")
+    mcdrserver.logger.info("或者用 MPM")
     server.register_help_message(
         '!!server',
         {
@@ -60,13 +78,13 @@ def on_load(server: PluginServerInterface, prev_module):
             then(
             Literal('stop').
                 requires(lambda src: src.has_permission(permissions['stop'])).
-                runs(lambda src: server.stop())
+                runs(lambda src: stop_server())
         ).
             then(
             Literal('stop_exit').
                 requires(
                 lambda src: src.has_permission(permissions['stop_exit'])).
-                runs(lambda src: server.stop_exit())
+                runs(lambda src: stop_exit_server())
         ).
             then(
             Literal('restart').
@@ -78,5 +96,10 @@ def on_load(server: PluginServerInterface, prev_module):
             Literal('exit').
                 requires(lambda src: src.has_permission(permissions['exit'])).
                 runs(lambda src: server.exit())
+        ).
+            then(
+            Literal('kill').
+                requires(lambda src: src.has_permission(permissions['exit'])).
+                runs(lambda src: server.kill())
         )
     )
