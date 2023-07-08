08a9e1846fcec8319e6356b8054a078d3063e85a
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-07-08 16:19:51 +0800

    Update __init__.py

--- StartStopHelper-v0.1.0/start_stop_helper_r_changed/__init__.py
+++ StartStopHelper-v0.1.0/start_stop_helper_r_changed/__init__.py
@@ -24,42 +24,75 @@ class Config(Serializable):
 config: Config
 
 command = {
-    "name": "!!server",
-    "requires": "lambda src: src.has_permission(permissions['help'])",
-    "runs": "lambda src: src.reply(server.rtr('start_stop_helper_r.help'))",
-    "children": [
+    'name': '!!server',
+    'requires': "permissions['help']",
+    'runs': "src.reply(server.rtr('start_stop_helper_r.help'))",
+    'children': [
         {
-            "name": "start",
-            "requires": "lambda src: src.has_permission(permissions['start'])",
-            "runs": "lambda src: server.start()"
+            'name': 'start',
+            'requires': "permissions['start']",
+            'runs': 'server.start()'
         },
         {
-            "name": "stop",
-            "requires": "lambda src: src.has_permission(permissions['stop'])",
-            "runs": "lambda src: stop_server()"
+            'name': 'stop',
+            'requires': "permissions['stop']",
+            'runs': 'stop_server()'
         },
         {
-            "name": "restart",
-            "requires": "lambda src: src.has_permission(permissions['restart'])",
-            "runs": "lambda src: server.restart()"
+            'name': 'restart',
+            'requires': "permissions['restart']",
+            'runs': 'server.restart()'
         },
         {
-            "name": "stop_exit",
-            "requires": "lambda src: src.has_permission(permissions['stop_exit'])",
-            "runs": "lambda src: stop_exit_server()"
+            'name': 'stop_exit',
+            'requires': "permissions['stop_exit']",
+            'runs': 'stop_exit_server()'
         },
         {
-            "name": "kill",
-            "requires": "lambda src: src.has_permission(permissions['kill'])",
-            "runs": "lambda src: server.kill()"
+            'name': 'kill',
+            'requires': "permissions['kill']",
+            'runs': 'server.kill()'
         },
         {
-            "name": "exit",
-            "requires": "lambda src: src.has_permission(permissions['exit'])",
-            "runs": "lambda src: server.exit()"
+            'name': 'exit',
+            'requires': "permissions['exit']",
+            'runs': 'server.exit()'
         },
     ]
 }
+
+
+def stop_server():
+
+    mcdrserver.stop()
+    time.sleep(10)
+
+    if server.is_server_running() == True:
+        time.sleep(60)
+        if mcdrserver.is_server_running() == True:
+
+            mcdrserver.logger.warning("服务器似乎并没有关闭...")
+            mcdrserver.logger.info("10s后服务端进程将被杀死.")
+            time.sleep(10)
+            mcdrserver.set_exit_after_stop_flag(False)
+            if mcdrserver.is_server_running() == False:
+                mcdrserver.logger.info("服务端已关闭")
+            else:
+                mcdrserver.set_exit_after_stop_flag(False)
+                mcdrserver.kill()
+
+    mcdrserver.logger.info("服务端已关闭.")
+
+
+def stop_exit_server():
+    stop_server()
+    mcdrserver.exit()
+
+
+def restart_server():
+    stop_server()
+    mcdrserver.start()
+
 
 def on_load(server: mcdrserver, prev_module):
     global config
@@ -71,35 +104,9 @@ def on_load(server: mcdrserver, prev_module):
     server.logger.info("https://www.mcdreforged.org/plugins/start_stop_helper_r")
     server.logger.info("或者用 MPM")
 
-    def stop_server():
-        mcdrserver.stop()
-        time.sleep(10)
-        if server.is_server_running() == True:
-            time.sleep(60)
-            if mcdrserver.is_server_running() == True:
-                mcdrserver.logger.warning("服务器似乎并没有关闭...")
-                mcdrserver.logger.info("10s后服务端进程将被杀死.")
-                time.sleep(10)
-                mcdrserver.set_exit_after_stop_flag(False)
-                if mcdrserver.is_server_running() == False:
-                    mcdrserver.logger.info("服务端已关闭")
-                else:
-                    mcdrserver.set_exit_after_stop_flag(False)
-                    mcdrserver.kill()
-        mcdrserver.logger.info("服务端已关闭.")
-
-
-    def stop_exit_server():
-        stop_server()
-        mcdrserver.exit()
-
-
-    def restart_server():
-        stop_server()
-        mcdrserver.start()
-
     register(server, command, '开关服助手')
-    """
+
+    '''
     server.register_help_message(
         '!!server',
         {
@@ -145,4 +152,4 @@ def on_load(server: mcdrserver, prev_module):
                 runs(lambda src: server.kill())
         )
     )
-    """
+    '''
